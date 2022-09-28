5fa14c917b9b21c1f96c8c5745195fef037614af
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-28 16:46:21 -0300

    feat: adiciona comentário da ajuda do colega Gustavo

--- app/backend/src/model/SequelizeModelUser.ts
+++ app/backend/src/model/SequelizeModelUser.ts
@@ -1,6 +1,8 @@
 import { ICreateUser } from '../interfaces/IUser';
 import User from '../database/models/User';
 
+// Utilização de um class abstract com model fixo para resolução da
+// model provienente da ajuda do Colega de turma Gustavo Silva
 abstract class SequelizeModelUser {
   protected _model = User;
 
