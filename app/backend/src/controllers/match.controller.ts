8687b0e4203dc07306ebc1ed59c1473f8d9f0249
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-29 18:19:30 -0300

    fix: correção da rota GET /matches com filter inProgress

--- app/backend/src/controllers/match.controller.ts
+++ app/backend/src/controllers/match.controller.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import { ICreateMatch } from '../interfaces/IMatch';
 import MatchService from '../services/match.service';
 
 export default class MatchController {
@@ -12,7 +11,7 @@ export default class MatchController {
       return next({ code, message });
     }
     if (inProgress === 'true' || inProgress === 'false') {
-      const matchesFilter = await this.service.findAll().filter((match) => match.inProgress === Boolean(inProgress));
+      const matchesFilter = data?.filter((match) => match.inProgress === Boolean(inProgress));
       return { code: 200, data: matchesFilter };
     }
     return res.status(code).json(data);
--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -1,10 +1,11 @@
 import MatchModel from '../model/MatchModel';
+import { ICreateMatch } from '../interfaces/IMatch';
 
 export default class MatchService {
   constructor(private model = new MatchModel()) {}
 
   public async findAll() {
-    const matches = await this.model.findAll();
+    const matches = await this.model.findAll() as unknown as ICreateMatch[];
     if (!matches) {
       return { code: 404, message: 'Not found any matches' };
     }
