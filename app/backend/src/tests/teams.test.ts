12e1d5d23190cde58a6d92c0ceb308b445ff596e
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-29 12:32:34 -0300

    feat: implementação dos testes para rota /teams

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,55 +1,104 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// // @ts-ignore
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
 
-// import { app } from '../app';
+import { app } from '../app';
 
-// import UserModel from '../model/UserModel';
+import TeamModel from '../model/TeamModel';
 
-// // import { Response } from 'superagent';
+chai.use(chaiHttp);
 
-// chai.use(chaiHttp);
+const { expect } = chai;
 
-// const { expect } = chai;
+describe('Rota /teams', () => {
 
-// describe('Rota /login', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
+  describe('Rota GET /', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findAll")
+        .resolves([
+          { id: 1, teamName: 'Botafogo' },
+          { id: 2, teamName: 'Corinthians' },
+          { id: 3, teamName: 'Cruzeiro' },
+        ]);
+    });
+  
+    after(() => {
+      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
+    });
+  
+  
+    it('Caso de sucesso', async () => {
+      const result = await chai.request(app).get('/teams');
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.be.a('array');
+      expect(result.body).to.have.length(3);
+      expect(result.body[0]).to.have.property('id');
+      expect(result.body[0]).to.have.property('teamName');
+    });
+  });
 
-//   // let chaiHttpResponse: Response;
+  describe('Rota GET /', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findAll")
+        .resolves(null);
+    });
+  
+    after(() => {
+      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
+    });
+  
+  
+    it('Caso de falha', async () => {
+      const result = await chai.request(app).get('/teams');
+      expect(result.status).to.be.equal(404);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('Not found any team');
+    });
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub(UserModel.prototype, "findOne")
-//       .resolves({
-//         id: 1,
-//         username: 'Admin',
-//         role: 'admin',
-//         email: 'admin@admin.com',
-//         password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
-//       });
-//   });
+  describe('Rota GET /:id', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findByPk")
+        .resolves({ id: 1, teamName: 'Botafogo' });
+    });
+  
+    after(() => {
+      (TeamModel.prototype.findByPk as sinon.SinonStub).restore();
+    });
+  
+  
+    it('Caso de sucesso', async () => {
+      const result = await chai.request(app).get('/teams/1');    
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('id');
+      expect(result.body).to.have.property('teamName');
+    });
+  });
 
-//   after(() => {
-//     (UserModel.prototype.findOne as sinon.SinonStub).restore();
-//   });
-
-//   // it('...', async () => {
-//   //   chaiHttpResponse = await chai
-//   //      .request(app)
-//   //      ...
-
-//   //   expect(...)
-//   // });
-
-//   it('Rota POST', async () => {
-//     const result = await chai.request(app).post('/login').send({
-//       email: 'admin@admin.com',
-//       password: 'secret_admin'
-//     });
-//     expect(result.status).to.be.equal(200);  
-//     expect(result.body).to.have.property('token');
-//   });
-// });
+  describe('Rota GET /:id', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findByPk")
+        .resolves(null);
+    });
+  
+    after(() => {
+      (TeamModel.prototype.findByPk as sinon.SinonStub).restore();
+    });
+  
+  
+    it('Caso de falha', async () => {
+      const result = await chai.request(app).get('/teams/1');
+      expect(result.status).to.be.equal(404);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('Team Not found');
+    });
+  });
+});
