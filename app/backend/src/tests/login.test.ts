82a29f399f8606fa0482777103cceb0854a64cc9
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-29 15:45:57 -0300

    refactor: simplificação dos testes de integração

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -13,43 +13,109 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const VALID_USER = {
+  id: 1,
+  username: 'Admin',
+  role: 'admin',
+  email: 'admin@admin.com',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+};
+
 describe('Rota /login', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
-  // let chaiHttpResponse: Response;
-
-  before(async () => {
-    sinon
-      .stub(UserModel.prototype, "findOne")
-      .resolves({
-        id: 1,
-        username: 'Admin',
-        role: 'admin',
+  describe('Rota POST /', () => {
+    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
+    after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
+  
+    it('Caso de sucesso', async () => {
+      const result = await chai.request(app).post('/login').send({
         email: 'admin@admin.com',
-        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+        password: 'secret_admin'
       });
-  });
-
-  after(() => {
-    (UserModel.prototype.findOne as sinon.SinonStub).restore();
-  });
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Rota POST', async () => {
-    const result = await chai.request(app).post('/login').send({
-      email: 'admin@admin.com',
-      password: 'secret_admin'
+      expect(result.status).to.be.equal(200);  
+      expect(result.body).to.have.property('token');
     });
-    expect(result.status).to.be.equal(200);  
-    expect(result.body).to.have.property('token');
+  });
+
+  describe('Rota POST /', () => {
+    it('Caso de falha - Campo email vazio', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: '',
+        password: 'secret_admin'
+      });
+      expect(result.status).to.be.equal(400);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('All fields must be filled');
+    });
+
+    it('Caso de falha - Campo email inválido', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: 'emailErrado',
+        password: 'secret_admin'
+      });
+      expect(result.status).to.be.equal(400);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('Incorrect email or password');
+    });
+
+    it('Caso de falha - Campo password inválido', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: 'admin@admin.com',
+        password: '123'
+      });
+      expect(result.status).to.be.equal(400);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('All fields must be filled');
+    });
+
+  });
+
+  describe('Rota POST /', () => {
+    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(null));
+    after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
+  
+    it('Caso de falha - Usuário não existente', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: 'admin2@admin.com',
+        password: 'secret_admin'
+      });
+      expect(result.status).to.be.equal(401);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('Incorrect email or password');
+    });
+  });
+
+  describe('Rota POST /', () => {
+    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
+    after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
+  
+    it('Caso de falha - Password errado', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: 'admin@admin.com',
+        password: 'secret_admin2'
+      });
+      expect(result.status).to.be.equal(401);
+      expect(result.body).to.be.a('object');
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('Incorrect email or password');
+    });
+  });
+
+
+  describe('Rota GET /validate', () => {
+    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
+    after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
+
+    it('Caso de sucesso', async () => {
+      const result = await chai.request(app).post('/login').send({
+        email: 'admin@admin.com',
+        password: 'secret_admin'
+      });
+      expect(result.status).to.be.equal(200);  
+      expect(result.body).to.have.property('token');
+    });
   });
 });
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -11,23 +11,17 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const ARRAY_TEAMS = [
+  { id: 1, teamName: 'Botafogo' },
+  { id: 2, teamName: 'Corinthians' },
+  { id: 3, teamName: 'Cruzeiro' },
+];
+
 describe('Rota /teams', () => {
 
   describe('Rota GET /', () => {
-    before(async () => {
-      sinon
-        .stub(TeamModel.prototype, "findAll")
-        .resolves([
-          { id: 1, teamName: 'Botafogo' },
-          { id: 2, teamName: 'Corinthians' },
-          { id: 3, teamName: 'Cruzeiro' },
-        ]);
-    });
-  
-    after(() => {
-      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
-    });
-  
+    before(async () => sinon.stub(TeamModel.prototype, "findAll").resolves(ARRAY_TEAMS));
+    after(() => (TeamModel.prototype.findAll as sinon.SinonStub).restore());
   
     it('Caso de sucesso', async () => {
       const result = await chai.request(app).get('/teams');
@@ -40,16 +34,8 @@ describe('Rota /teams', () => {
   });
 
   describe('Rota GET /', () => {
-    before(async () => {
-      sinon
-        .stub(TeamModel.prototype, "findAll")
-        .resolves(null);
-    });
-  
-    after(() => {
-      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
-    });
-  
+    before(async () => sinon.stub(TeamModel.prototype, "findAll").resolves(null));
+    after(() => (TeamModel.prototype.findAll as sinon.SinonStub).restore());
   
     it('Caso de falha', async () => {
       const result = await chai.request(app).get('/teams');
@@ -61,17 +47,9 @@ describe('Rota /teams', () => {
   });
 
   describe('Rota GET /:id', () => {
-    before(async () => {
-      sinon
-        .stub(TeamModel.prototype, "findByPk")
-        .resolves({ id: 1, teamName: 'Botafogo' });
-    });
-  
-    after(() => {
-      (TeamModel.prototype.findByPk as sinon.SinonStub).restore();
-    });
-  
-  
+    before(async () => sinon.stub(TeamModel.prototype, "findByPk").resolves(ARRAY_TEAMS[0]));
+    after(() => (TeamModel.prototype.findByPk as sinon.SinonStub).restore());
+
     it('Caso de sucesso', async () => {
       const result = await chai.request(app).get('/teams/1');    
       expect(result.status).to.be.equal(200);
@@ -82,16 +60,8 @@ describe('Rota /teams', () => {
   });
 
   describe('Rota GET /:id', () => {
-    before(async () => {
-      sinon
-        .stub(TeamModel.prototype, "findByPk")
-        .resolves(null);
-    });
-  
-    after(() => {
-      (TeamModel.prototype.findByPk as sinon.SinonStub).restore();
-    });
-  
+    before(async () => sinon.stub(TeamModel.prototype, "findByPk").resolves(null));
+    after(() => (TeamModel.prototype.findByPk as sinon.SinonStub).restore());
   
     it('Caso de falha', async () => {
       const result = await chai.request(app).get('/teams/1');
