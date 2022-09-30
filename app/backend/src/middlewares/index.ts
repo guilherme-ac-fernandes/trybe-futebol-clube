169cbdfca023b202f622a0b0f1a6b00568ca318d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-29 22:53:50 -0300

    feat: criação do middleware para verificação das partidas

--- app/backend/src/middlewares/index.ts
+++ app/backend/src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import error from './error';
 import login from './login.middleware';
 import auth from './auth';
+import match from './match.middleware';
 
-export default { error, login, auth };
+export default { error, login, auth, match };
---
+++ app/backend/src/middlewares/match.middleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express';
+import MatchService from '../services/match.service';
+import { IMatch } from '../interfaces/IMatch';
+
+export default async (req: Request, _res: Response, next: NextFunction) => {
+  const { homeTeam, awayTeam } = req.body as IMatch;
+
+  if (homeTeam === awayTeam) {
+    return next({
+      code: 401,
+      message: 'It is not possible to create a match with two equal teams',
+    });
+  }
+
+  const service = new MatchService();
+  const { data: team1 } = await service.findOne(homeTeam);
+  const { data: team2 } = await service.findOne(homeTeam);
+  if (!team1 || !team2) {
+    return next({
+      code: 404,
+      message: 'There is no team with such id!',
+    });
+  }
+
+  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // if (!(email.match(emailRegex))) {
+  //   return next({ code: 400, message: 'Incorrect email or password' });
+  // }
+
+  // if (!password || password.length < 7) {
+  //   return next({ code: 400, message: 'All fields must be filled' });
+  // }
+
+  next();
+};
