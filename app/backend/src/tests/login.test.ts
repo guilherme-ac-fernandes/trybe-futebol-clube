9f3be44a69b805d09fd85890f7fa4cc8b03ec486
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-29 23:47:10 -0300

    feat: implementação dos testes da rota GET /login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,5 +1,6 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import * as jwt from 'jsonwebtoken';
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
@@ -7,31 +8,39 @@ import { app } from '../app';
 
 import UserModel from '../model/UserModel';
 
-// import { Response } from 'superagent';
-
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-const VALID_USER = {
+const ADMIN_USER = {
   id: 1,
   username: 'Admin',
   role: 'admin',
   email: 'admin@admin.com',
-  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
+};
+
+const NOT_ADMIN_USER = {
+  id: 2,
+  username: 'User',
+  role: 'user',
+  email: 'user@user.com',
+  password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',
 };
 
 describe('Rota /login', () => {
   describe('Rota POST /', () => {
-    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
+    before(async () =>
+      sinon.stub(UserModel.prototype, 'findOne').resolves(ADMIN_USER)
+    );
     after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
-  
+
     it('Caso de sucesso', async () => {
       const result = await chai.request(app).post('/login').send({
         email: 'admin@admin.com',
-        password: 'secret_admin'
+        password: 'secret_admin',
       });
-      expect(result.status).to.be.equal(200);  
+      expect(result.status).to.be.equal(200);
       expect(result.body).to.have.property('token');
     });
   });
@@ -40,7 +49,7 @@ describe('Rota /login', () => {
     it('Caso de falha - Campo email vazio', async () => {
       const result = await chai.request(app).post('/login').send({
         email: '',
-        password: 'secret_admin'
+        password: 'secret_admin',
       });
       expect(result.status).to.be.equal(400);
       expect(result.body).to.be.a('object');
@@ -51,7 +60,7 @@ describe('Rota /login', () => {
     it('Caso de falha - Campo email inválido', async () => {
       const result = await chai.request(app).post('/login').send({
         email: 'emailErrado',
-        password: 'secret_admin'
+        password: 'secret_admin',
       });
       expect(result.status).to.be.equal(400);
       expect(result.body).to.be.a('object');
@@ -62,24 +71,25 @@ describe('Rota /login', () => {
     it('Caso de falha - Campo password inválido', async () => {
       const result = await chai.request(app).post('/login').send({
         email: 'admin@admin.com',
-        password: '123'
+        password: '123',
       });
       expect(result.status).to.be.equal(400);
       expect(result.body).to.be.a('object');
       expect(result.body).to.have.property('message');
       expect(result.body.message).to.be.equal('All fields must be filled');
     });
-
   });
 
   describe('Rota POST /', () => {
-    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(null));
+    before(async () =>
+      sinon.stub(UserModel.prototype, 'findOne').resolves(null)
+    );
     after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
-  
+
     it('Caso de falha - Usuário não existente', async () => {
       const result = await chai.request(app).post('/login').send({
         email: 'admin2@admin.com',
-        password: 'secret_admin'
+        password: 'secret_admin',
       });
       expect(result.status).to.be.equal(401);
       expect(result.body).to.be.a('object');
@@ -89,13 +99,15 @@ describe('Rota /login', () => {
   });
 
   describe('Rota POST /', () => {
-    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
+    before(async () =>
+      sinon.stub(UserModel.prototype, 'findOne').resolves(ADMIN_USER)
+    );
     after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
-  
+
     it('Caso de falha - Password errado', async () => {
       const result = await chai.request(app).post('/login').send({
         email: 'admin@admin.com',
-        password: 'secret_admin2'
+        password: 'secret_admin2',
       });
       expect(result.status).to.be.equal(401);
       expect(result.body).to.be.a('object');
@@ -104,18 +116,79 @@ describe('Rota /login', () => {
     });
   });
 
-
   describe('Rota GET /validate', () => {
-    before(async () => sinon.stub(UserModel.prototype, "findOne").resolves(VALID_USER));
-    after(() => (UserModel.prototype.findOne as sinon.SinonStub).restore());
+    before(async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => {
+        return Promise.resolve({ success: 'Token is valid' });
+      });
+      sinon.stub(UserModel.prototype, 'findOne').resolves(ADMIN_USER);
+    });
+    after(() => {
+      (UserModel.prototype.findOne as sinon.SinonStub).restore();
+      (jwt.verify as sinon.SinonStub).restore();
+    });
 
     it('Caso de sucesso', async () => {
-      const result = await chai.request(app).post('/login').send({
-        email: 'admin@admin.com',
-        password: 'secret_admin'
-      });
-      expect(result.status).to.be.equal(200);  
-      expect(result.body).to.have.property('token');
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2NDUwNDc4MCwiZXhwIjoxNjY0NTkxMTgwfQ.lYN2ImWYl-ejFGAMEClZzcFS6I3Bx4PX2lfS47v9rus';
+      const result = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', token);
+
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.have.property('role');
+      expect(result.body.role).to.be.equal('admin');
     });
   });
+
+  describe('Rota GET /validate', () => {
+    before(async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => {
+        return Promise.resolve({ success: 'Token is valid' });
+      });
+      sinon.stub(UserModel.prototype, 'findOne').resolves(ADMIN_USER);
+    });
+    after(() => {
+      (UserModel.prototype.findOne as sinon.SinonStub).restore();
+      (jwt.verify as sinon.SinonStub).restore();
+    });
+
+    it('Caso de Administrador', async () => {
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2NDUwNDc4MCwiZXhwIjoxNjY0NTkxMTgwfQ.lYN2ImWYl-ejFGAMEClZzcFS6I3Bx4PX2lfS47v9rus';
+      const result = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', token);
+
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.have.property('role');
+      expect(result.body.role).to.be.equal('admin');
+    });
+  });
+
+  describe('Rota GET /validate', () => {
+    before(async () => {
+      sinon.stub(jwt, 'verify').callsFake(() => {
+        return Promise.resolve({ success: 'Token is valid' });
+      });
+      sinon.stub(UserModel.prototype, 'findOne').resolves(NOT_ADMIN_USER);
+    });
+    after(() => {
+      (UserModel.prototype.findOne as sinon.SinonStub).restore();
+      (jwt.verify as sinon.SinonStub).restore();
+    });
+
+    it('Caso de não Administrador', async () => {
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2NDUwNDc4MCwiZXhwIjoxNjY0NTkxMTgwfQ.lYN2ImWYl-ejFGAMEClZzcFS6I3Bx4PX2lfS47v9rus';
+      const result = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', token);
+
+      expect(result.status).to.be.equal(401);
+      expect(result.body).to.have.property('message');
+      expect(result.body.message).to.be.equal('User not Admin');
+    });
+  });
+
 });
