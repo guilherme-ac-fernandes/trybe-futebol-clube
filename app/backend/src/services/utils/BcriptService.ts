e13e86851a2db61554d60348e403eb8199bb2711
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-02 21:14:14 -0300

    refactor: adiciona comentário da fonte da função do Bcript

--- app/backend/src/services/utils/BcriptService.ts
+++ app/backend/src/services/utils/BcriptService.ts
@@ -1,13 +1,16 @@
-import * as bcrypt from 'bcryptjs';
+import { hashSync, compareSync } from 'bcryptjs';
 
+// Função baseada na resolução do exercício da mentoria de revisão do Especialista Lucas Henrique de Abreu
+// presente do repositório das aulas ao vivo da turma 20B
+// source: https://github.com/tryber/sd-020-b-live-lectures/blob/lectures-solid-poo-tdd-typescript-sequelize/src/services/utils/BcriptService.ts
 export default class BcryptService {
   private static salt = 10;
 
   public static encrypt(text: string): string {
-    return bcrypt.hashSync(text, this.salt);
+    return hashSync(text, this.salt);
   }
 
   public static compare(encryptText: string, planText: string): boolean {
-    return bcrypt.compareSync(planText, encryptText);
+    return compareSync(planText, encryptText);
   }
 }
