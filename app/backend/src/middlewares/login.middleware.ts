226bf36d007b2813702eb5102575f59fc560f1d4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-04 00:34:29 -0300

    refactor: remoção de linhas vazias dos middleware

--- app/backend/src/middlewares/login.middleware.ts
+++ app/backend/src/middlewares/login.middleware.ts
@@ -3,19 +3,15 @@ import { ILogin } from '../interfaces/IUser';
 
 export default (req: Request, _res: Response, next: NextFunction) => {
   const { email, password } = req.body as ILogin;
-
   if (!email || email.length === 0) {
     return next({ code: 400, message: 'All fields must be filled' });
   }
-
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!(email.match(emailRegex))) {
     return next({ code: 400, message: 'Incorrect email or password' });
   }
-
   if (!password || password.length < 7) {
     return next({ code: 400, message: 'All fields must be filled' });
   }
-
   next();
 };
--- app/backend/src/middlewares/match.middleware.ts
+++ app/backend/src/middlewares/match.middleware.ts
@@ -4,14 +4,12 @@ import { IMatch } from '../interfaces/IMatch';
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body as IMatch;
-
   if (homeTeam === awayTeam) {
     return next({
       code: 401,
       message: 'It is not possible to create a match with two equal teams',
     });
   }
-
   const service = new TeamService();
   const { data: team1 } = await service.findByPk(homeTeam);
   const { data: team2 } = await service.findByPk(homeTeam);
